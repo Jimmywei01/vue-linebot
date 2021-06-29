f6acde7cee409c3713f1e390c312cb149c78ab70
吉米 <min1990719@gmail.com>
2021-06-29 22:26:04 +0800

    Update botRoute.js

--- server/routes/line/botRoute.js
+++ server/routes/line/botRoute.js
@@ -4,10 +4,8 @@ const router = express.Router()
 const line = require('@line/bot-sdk');
 
 router.post('/linebot', line.middleware({
-    // channelSecret: process.env.channelSecret,
-    // channelAccessToken: process.env.channelAccessToken
-    channelSecret: '30665550df41b856ad9f63d73ca29c6b',
-    channelAccessToken: 'GU8oaldUQoE7Kec9LxO0OgGUxCgtrikF+eEdEVOcf/aqv0ZWSdRhhBSaoi1CAR5rOyudfV1Guad5hbxrJdEpyXTJWBplVKfCc17BWZLTNl//tBqdykfg9pWs2A02QNU+jnJzrXoyRr1A2IVgUbsKkQdB04t89/1O/w1cDnyilFU='
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken:process.env.CHANNEL_ACCESS_TOKEN
 }), botController.call);
 
 
