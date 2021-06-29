752d6fb2dec2949c19e17c2eb1c00bac6c3e9f66
吉米 <min1990719@gmail.com>
2021-06-29 22:19:06 +0800

    Update auth.config.js

--- server/config/auth.config.js
+++ server/config/auth.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   screat:"jimmy-secret-key",
   IMGUR_CUSTOMER_KEY: 'eaf909b5ce76352',
-  imgURl: 'https://api.imgur.com/3/upload',
-  // channelAccessToken: 'GU8oaldUQoE7Kec9LxO0OgGUxCgtrikF+eEdEVOcf/aqv0ZWSdRhhBSaoi1CAR5rOyudfV1Guad5hbxrJdEpyXTJWBplVKfCc17BWZLTNl//tBqdykfg9pWs2A02QNU+jnJzrXoyRr1A2IVgUbsKkQdB04t89/1O/w1cDnyilFU=',
-  // channelSecret: '30665550df41b856ad9f63d73ca29c6b'
-  // channelAccessToken: process.env.channelAccessToken,
-  // channelSecret: process.env.channelSecret,
-  // channelId: process.env.channelId
+  imgURl: 'https://api.imgur.com/3/upload'
 }
