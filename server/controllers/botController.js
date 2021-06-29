d0efa923daa845c661f72651f880adfc32645c0c
吉米 <min1990719@gmail.com>
2021-06-29 22:23:58 +0800

    Update botController.js

--- server/controllers/botController.js
+++ server/controllers/botController.js
@@ -10,9 +10,8 @@ const line = require("@line/bot-sdk")
 
 // create LINE SDK client
 const client = new line.Client({
-  channelSecret: "30665550df41b856ad9f63d73ca29c6b",
-  channelAccessToken:
-    "GU8oaldUQoE7Kec9LxO0OgGUxCgtrikF+eEdEVOcf/aqv0ZWSdRhhBSaoi1CAR5rOyudfV1Guad5hbxrJdEpyXTJWBplVKfCc17BWZLTNl//tBqdykfg9pWs2A02QNU+jnJzrXoyRr1A2IVgUbsKkQdB04t89/1O/w1cDnyilFU=",
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken:process.env.CHANNEL_ACCESS_TOKEN
 })
 
 botController = {
