e4a188ef85ecc1e791dc223a436b846b062dc6b7
吉米 <min1990719@gmail.com>
2021-06-29 22:22:36 +0800

    Update auth.config.js

--- server/config/auth.config.js
+++ server/config/auth.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  screat:"jimmy-secret-key",
-  IMGUR_CUSTOMER_KEY: 'eaf909b5ce76352',
+  screat: process.env.SCREAT,
+  IMGUR_CUSTOMER_KEY: process.env.IMGUR_CUSTOMER_KEY,
   imgURl: 'https://api.imgur.com/3/upload'
 }
